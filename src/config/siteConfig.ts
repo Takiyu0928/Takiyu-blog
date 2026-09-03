f0f72fb604c5eba7a0e75da20b3b5b9d45c48643
Takiyu <takiyu0928@gmail.com>
2026-09-03 15:06:27 +0800

    添加桌面端横幅图片

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -71,6 +71,7 @@ export const siteConfig: SiteConfig = {
 			desktop: [
 				"/assets/desktop-banner/1788419164683-1.jpg",
 				"/assets/desktop-banner/1788419175198-2.jpg",
+				"/assets/desktop-banner/1788419185208-3.jpg",
 			],
 			mobile: [
 				"/assets/mobile-banner/1788418737181-6.png",
